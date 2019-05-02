811b5125b9285d346beb899383f9e639453b74d8
Joan Varon <joan@timemanagerweb.com>
2019-05-02 15:11:14 -0500

    got it barely working

--- mirage/config.js
+++ mirage/config.js
@@ -7,10 +7,43 @@ export default function() {
         type: 'users',
         id: '1',
         attributes: {
-          name: 'Grand Old Mansion',
-          id: 'Veruca Salt',
-          image: 'San Francisco',
-          mood: 'San Francisco',
+          name: 'Pepe Andres',
+          mood: 10,
+          image: 'https://picsum.photos/59',
+        }
+      }, {
+        type: 'users',
+        id: '2',
+        attributes: {
+          name: 'Juan Patracio',
+          mood: 12,
+          image: 'https://picsum.photos/60',
+        }
+      }, {
+        type: 'users',
+        id: '3',
+        attributes: {
+          name: 'Antonio Benz',
+          mood: 21,
+          image: 'https://picsum.photos/61',
+        }
+      },
+      {
+        type: 'users',
+        id: '4',
+        attributes: {
+          name: 'Pepe Andres',
+          mood: 79,
+          image: 'https://picsum.photos/62',
+        }
+      },
+      {
+        type: 'users',
+        id: '5',
+        attributes: {
+          name: 'San Andres',
+          mood: 2,
+          image: 'https://picsum.photos/58',
         }
       }]
     };
